903b5ee7a2f72c47e3b49583eb281c1ba8863044
Nay Chi Cho <naychicho.dev@gmail.com>
2026-09-23 19:46:10 +0630

    docker test

--- src/main/java/com/napier/App.java
+++ src/main/java/com/napier/App.java
@@ -1,43 +1,62 @@
 package com.napier;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
-public class App {
-    private Connection con = null;
-
-    public void connect() {
-        try {
+public class App
+{
+    public static void main(String[] args)
+    {
+        try
+        {
+            // Load Database driver
             Class.forName("com.mysql.cj.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
+        }
+        catch (ClassNotFoundException e)
+        {
             System.out.println("Could not load SQL driver");
             System.exit(-1);
         }
 
-        String host = System.getenv("DB_HOST");
-        if (host == null) {
-            host = "db";
-        }
-
-        int retries = 10;
-        for (int i = 0; i < retries; ++i) {
+        // Connection to the database
+        Connection con = null;
+        int retries = 100;
+        for (int i = 0; i < retries; ++i)
+        {
             System.out.println("Connecting to database...");
-            try {
-                Thread.sleep(5000);
-                con = DriverManager.getConnection("jdbc:mysql://" + host + ":3306/world?useSSL=false", "root", "password");
-                System.out.println("Successfully Connected");
+            try
+            {
+                // Wait a bit for db to start
+                Thread.sleep(30000);
+                // Connect to database
+                con = DriverManager.getConnection("jdbc:mysql://db:3306/employees?allowPublicKeyRetrieval=true&useSSL=false", "root", "example");
+                System.out.println("Successfully connected");
+                // Wait a bit
+                Thread.sleep(10000);
+                // Exit for loop
                 break;
-            } catch (SQLException sqle) {
-                System.out.println("Failed to connect to database attempt " + i);
-            } catch (InterruptedException ie) {
-                System.out.println("Thread interrupted?");
+            }
+            catch (SQLException sqle)
+            {
+                System.out.println("Failed to connect to database attempt " + Integer.toString(i));
+                System.out.println(sqle.getMessage());
+            }
+            catch (InterruptedException ie)
+            {
+                System.out.println("Thread interrupted? Should not happen.");
             }
         }
-    }
 
-    public static void main(String[] args) {
-        App a = new App();
-        a.connect();
+        if (con != null)
+        {
+            try
+            {
+                // Close connection
+                con.close();
+            }
+            catch (Exception e)
+            {
+                System.out.println("Error closing connection to database");
+            }
+        }
     }
 }
